afff222c8db6866370d15b7db520e7442f1b4551
lijian <852688838@qq.com>
2018-06-27 20:58:13 +0800

    Finish static pages

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/','StaticPagesController@home')->name('home');
+Route::get('/about','StaticPagesController@about')->name('about');
+Route::get('/help','StaticPagesController@help')->name('help');
